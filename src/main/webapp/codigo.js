2d83b86bd876514c456bf79989ec37ada253aaea
Hemmerson Luis Barros da Rosa <hemmerson.rosa@gmail.com>
2023-09-28 10:27:55 -0300

    functional project

--- src/main/webapp/codigo.js
+++ src/main/webapp/codigo.js
@@ -23,6 +23,17 @@ function listaMenu(doc) {
 }
 
 function preencheDados(receita){
+    let texto = "<h2 id=\"nome_receita\">Nome receita</h2>\n" +
+        "                <hr>\n" +
+        "                <h4>Ingredientes:</h4>\n" +
+        "                <ul id=\"ingredientes\"></ul>\n" +
+        "                <hr>\n" +
+        "                <h4>Modo de Preparo:</h4>\n" +
+        "                <ol id=\"modo_preparo\"></ol>\n" +
+        "                <hr>\n" +
+        "                <h4>Ilustrações:</h4>\n" +
+        "                <div id=\"images\"></div>";
+    document.getElementById("principal").innerHTML = texto;
     console.log(receita);
     ajax(receita, function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -65,16 +76,25 @@ function mostraModoPreparo(doc){
 
 }
 
-function buscar(doc){
-    const receitas = doc.getElementsByTagName("receita");
-    let texto = "<ul>";
-    for(let receita of receitas){
-        texto += `<li>${receita.getElementsByTagName("nome")[0].textContent}</li>`
-    }
-    texto += "</ul>";
-    document.getElementById('principal').innerHTML=texto;
-
+function buscar(){
+    let pesquisa = document.getElementById("txt-pesquisar").value;
+    console.log(`buscaingrediente?ingrediente=${pesquisa}`);
+    ajax(`buscaingrediente?ingrediente=${pesquisa}`, function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const doc = this.responseXML;
+            const nomeReceitas = doc.getElementsByTagName("nome");
+            let texto = "";
+            if (nomeReceitas != null) {
+                for (let nome of nomeReceitas) {
+                    console.log(nome.firstChild.textContent);
+                    texto += `<h1>${nome.firstChild.textContent}</h1><hr>`;
+                }
+            }
+            document.getElementById("principal").innerHTML = texto;
+        }
+    });
 }
+
 
 function ajax(recurso, funcao) {
     const xhr = new XMLHttpRequest();
